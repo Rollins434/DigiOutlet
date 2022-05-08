3a64ba2bed77dfd53c33c6a6caa437236741e14a
Rupendra Aathiya <rupendra.aathiya@marutitech.com>
2022-05-08 20:23:04 +0530

    cart updates

--- src/Reducers/cartReducer.js
+++ src/Reducers/cartReducer.js
@@ -1,12 +1,14 @@
-import { CART_ADD_ITEM } from "../constants/cartConstant";
+import { CART_ADD_ITEM,CART_REMOVE_ITEM } from "../constants/cartConstant";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
+
   switch (action.type) {
+    
     case CART_ADD_ITEM:
         const item = action.payload
   
-        const existItem = state.cartItems.find((x) => x.product === item.product)
-  
+        const existItem = state.cartItems.find(x => x.product === item.product)
+
         if (existItem) {
           return {
             ...state,
@@ -20,7 +22,9 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
             cartItems: [...state.cartItems, item],
           }
         }
-
+        
+         
+    
         default:
             return state
         
